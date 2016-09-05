d0fdbd6be39da6a76333816072c9bc9405ebc3e7
carmen0208 <13770576@qq.com>
2016-09-05 15:16:45 +0800

    anonymous functions

---
+++ 5.anonymous-functions/execises.exs
@@ -0,0 +1,18 @@
+#list_concat.([:a, :b], [:c, :d]) #=> [:a, :b, :c, :d]
+#sum.(1, 2, 3) #=> 6
+#pair_tuple_to_list.( { 1234, 5678 } ) #=> [ 1234, 5678 ]<Paste>
+
+list_concat =fn(a, b)->	a ++ b end
+
+sum = fn(a,b,c)->
+	a + b + c
+end
+
+pair_tuple_to_list = fn({a,b})->
+	[a,b]
+end
+
+list_concat.([:a, :b], [:c, :d])
+sum.(1, 2, 3)
+pair_tuple_to_list.({1234,5678})
+
