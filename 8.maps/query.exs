df2fb6864cacf6cf7e09350aefeb10582c4ec104
carmen0208 <13770576@qq.com>
2016-09-06 18:44:42 +0800

    map

---
+++ 8.maps/query.exs
@@ -0,0 +1,11 @@
+people = [
+  %{ name: "Carmen", height: 1.24},
+  %{ name: "Grumpy",    height: 1.24 },
+  %{ name: "Dave",      height: 1.88 },
+  %{ name: "Dopey",     height: 1.32 },
+  %{ name: "Shaquille", height: 2.16 },
+  %{ name: "Sneezy",    height: 1.28 }
+]
+
+IO.inspect(for person = %{ height: height } <- people, height > 1.5, do: person )
+
