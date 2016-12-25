892b3c3f1efee2fbadc1276ed02778ee9f229704
Carmen Liu <carmen.liujia@gmail.com>
2016-12-25 17:48:31 +1300

    TDD for another function and refactoring it

---
+++ dailydrip/01-20/006_unit_testing.md
@@ -0,0 +1,19 @@
+## ExUnit
+
+Elixir's build-in Unit Testing framework
+
+* [[ExUnit.Assertions|http://elixir-lang.org/docs/stable/ex_unit/ExUnit.Assertions.html]]
+
+## Example(TDD)
+
+```sh
+mix new schizo
+```
+
+* [example](schizo/test/schizo_test.exs)
+
+## Run test
+
+```sh
+mix test
+```
--- dailydrip/01-20/schizo/lib/schizo.ex
+++ dailydrip/01-20/schizo/lib/schizo.ex
@@ -1,6 +1,29 @@
 defmodule Schizo do
+  @moduledoc """
+  A nice module that lets you uppercase or unvowel every other letter in a sentence
+  """
+
+  @doc """
+    Uppercases every other word in a sentence. Example:
+
+    iex> Schizo.uppercase("you are silly")
+    "you ARE silly"
+  """
   def uppercase(string) do
-    # string
+    transformed_every_other_word(string, &uppercaser/1)
+  end
+
+  @doc """
+    Removes vowels from every other word in a sentence, Example:
+
+    iex> Schizo.unvowel("you are silly")
+    "you r silly"
+  """
+  def unvowel(string) do
+    transformed_every_other_word(string, &unvoweler/1)
+  end
+
+  def transformed_every_other_word(string,transformation) do
     # Split string on spaces into a list of words
     # http://elixir-lang.org/docs/stable/elixir/String.html # split
     words = String.split(string)
@@ -8,16 +31,25 @@ defmodule Schizo do
     # http://elixir-lang.org/docs/stable/elixir/Stream.html#with_index/2
     words_with_index = Stream.with_index(words)
 
-    transformed_words = Enum.map(words_with_index, &uppercase_every_other_word/1)
+    transformed_words = Enum.map(words_with_index, transformation)
     # Join the words back with space
     #http://elixir-lang.org/docs/stable/elixir/Enum.html#join/2
     Enum.join(transformed_words, " ")
   end
+  def uppercaser(input) do
 
-  def uppercase_every_other_word({word, index}) do
+    transformer(input, &String.upcase/1)
+
+  end
+
+  def unvoweler(input) do
+    transformer(input, fn (word) -> Regex.replace(~r([aeiou]), word, "") end)
+  end
+
+  def transformer({word, index}, transformation) do
     cond do
       rem(index, 2) == 0 -> word
-      rem(index, 2) == 1 -> String.upcase(word)
+      rem(index, 2) == 1 -> transformation.(word)
     end
   end
 end
