45bb873becefe4fdd395094b1f60546232b43110
Carmen Liu <carmen.liujia@gmail.com>
2017-02-20 08:07:44 +1300

    test email changes is ready

---
+++ dailydrip/01-20/013_processes_p2.md
@@ -0,0 +1,9 @@
+## Process
+
+Process share no memory with one another, and can only communicate with each other via asynchronous messages
+
+## demo
+
+```sh
+mix new bank_account && cd bank_account
+```
---
+++ dailydrip/01-20/bank_account/test/bank_account_test.exs
@@ -0,0 +1,28 @@
+defmodule BankAccount do
+  def start do
+    await
+  end
+  def await do
+    receive do
+      {:check_balance, pid} -> divulge_balance(pid)
+    end
+    await
+  end
+
+  def divulge_balance(pid)
+
+  end
+end
+defmodule BankAccountTest do
+  use ExUnit.Case
+
+  test "starts off with a balance of 0" do
+    account = spawn_link(BankAccount, :start, [])
+    verify_balance_is 0, account
+  end
+
+  def verify_balance_is(expected_balance, account) do
+    send account, {:check_balance, self}
+    assert_receive {:balance, ^expected_balance}
+  end
+end
