ac363a63a80449f232171e42cd86c3507724d8b0
Carmen Liu <carmen.liujia@gmail.com>
2017-02-28 18:27:45 +1300

    Modeling a Bank Account with a Process: Deposit; Withdraw; Balance Inquiry; Command-Query Responsibility Segregation; Event Sourcing

--- dailydrip/01-20/bank_account/test/bank_account_test.exs
+++ dailydrip/01-20/bank_account/test/bank_account_test.exs
@@ -1,16 +1,49 @@
 defmodule BankAccount do
   def start do
-    await
+    await([])
   end
-  def await do
-    receive do
-      {:check_balance, pid} -> divulge_balance(pid)
-    end
-    await
+  def await(events) do
+    events =
+      receive do
+        {:check_balance, pid} -> divulge_balance(pid,events)
+        {:deposit, amount} -> deposit(amount, events)
+        {:withdraw, amount} -> withdraw(amount, events)
+      end
+    await(events)
   end
 
-  def divulge_balance(pid)
+  defp deposit(amount, events) do
+    events ++ [{:deposit, amount}]
+  end
 
+  defp withdraw(amount, events) do
+    events ++ [{:withdrawal, amount}]
+  end
+
+  defp divulge_balance(pid,events) do
+    send pid, {:balance, calculate_balance(events)}
+  end
+
+  defp calculate_balance(events) do
+    deposits = sum(just_deposits(events))
+    withdrawals = sum(just_withdrawals(events))
+    deposits - withdrawals
+  end
+
+  defp just_deposits(events) do
+    just_type(events, :deposit)
+  end
+
+  defp just_withdrawals(events) do
+    just_type(events, :withdrawal)
+  end
+
+  defp just_type(events, expected_type) do
+    Enum.filter(events, fn({type, _}) -> type == expected_type end )
+  end
+
+  defp sum(events) do
+    Enum.reduce(events, 0, fn({_, amount}, acc) -> acc + amount end )
   end
 end
 defmodule BankAccountTest do
@@ -19,6 +52,19 @@ defmodule BankAccountTest do
   test "starts off with a balance of 0" do
     account = spawn_link(BankAccount, :start, [])
     verify_balance_is 0, account
+  end
+
+  test "has balance increamented by the amount of a deposit" do
+    account = spawn_link(BankAccount, :start, [])
+    send account, {:deposit, 10}
+    verify_balance_is 10, account
+  end
+
+  test "has balance decremented by the amount of a withdraw" do
+    account = spawn_link(BankAccount, :start, [])
+    send account, {:deposit, 20}
+    send account, {:withdraw, 10}
+    verify_balance_is 10, account
   end
 
   def verify_balance_is(expected_balance, account) do
