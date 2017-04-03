96a9f4b9de826ae7dec17bfed3c5145ebaeb9869
Carmen Liu <carmen.liujia@gmail.com>
2017-04-03 20:53:25 +1200

    023 Supervisors and Persistent State

--- dailydrip/01-20/supervised_list_server/lib/list_server.ex
+++ dailydrip/01-20/supervised_list_server/lib/list_server.ex
@@ -3,55 +3,61 @@ defmodule ListServer do
   use GenServer
 
   ### Public API
-  def start_link do
-    GenServer.start_link(__MODULE__, [], name: :list)
+  def start_link(list_data_pid) do
+    :gen_server.start_link({:local, :list}, __MODULE__, list_data_pid, [])
+    #GenServer.start_link(__MODULE__, list_data_pid, name: :list)
   end
 
   def clear do
-    GenServer.cast :list, :clear
+    :gen_server.cast :list, :clear
   end
 
   def add(item) do
-    GenServer.cast :list, {:add, item}
+    :gen_server.cast :list, {:add, item}
   end
 
   def remove(item) do
-    GenServer.cast :list, {:remove, item}
+    :gen_server.cast :list, {:remove, item}
   end
 
   def items do
-    GenServer.call :list, :items
+    :gen_server.call :list, :items
   end
 
   def crash do
-    GenServer.cast :list, :crash
+    :gen_server.cast :list, :crash
   end
 
-  ### GenServer API
-  def init(list) do
-    {:ok, list}
+  ### :gen_server API
+  def init(list_data_pid) do
+    list = ListData.get_state(list_data_pid)
+    {:ok, {list, list_data_pid}}
   end
 
   # Clear the list
   #
-  def handle_cast(:crash, list) do
+  def handle_cast(:crash, _state) do
     1 = 2
   end
-  def handle_cast(:clear, list) do
-    {:noreply, []}
+  def handle_cast(:clear, {_list, list_data_pid}) do
+    {:noreply, {[], list_data_pid}}
   end
 
-  def handle_cast({:add, item}, list) do
-    {:noreply, list ++ [item]}
+  def handle_cast({:add, item}, {list, list_data_pid}) do
+    {:noreply, {list ++ [item], list_data_pid}}
   end
 
-  def handle_cast({:remove, item}, list) do
-    {:noreply, List.delete(list, item)}
+  def handle_cast({:remove, item}, {list, list_data_pid}) do
+    {:noreply, {List.delete(list, item), list_data_pid}}
   end
 
-  def handle_call(:items, _from, list) do
-    {:reply, list, list}
+  def handle_call(:items, _from, {list, list_data_pid}) do
+    {:reply, list, {list, list_data_pid}}
   end
 
+  # Handle termination
+  def terminate(_resone, {list, list_data_pid}) do 
+    ListData.save_state list_data_pid, list
+  end
 
 end
--- dailydrip/01-20/supervised_list_server/lib/list_supervisor.ex
+++ dailydrip/01-20/supervised_list_server/lib/list_supervisor.ex
@@ -2,11 +2,21 @@ defmodule ListSupervisor do
   use Supervisor
 
   def start_link do
-    :supervisor.start_link(__MODULE__, [])
+    result = {:ok, sup} = :supervisor.start_link(__MODULE__, [])
+    start_workers(sup)
+    result
   end
 
-  def init(list) do
-    child_processes = [ worker(ListServer, list) ]
-    supervise child_processes, strategy: :one_for_one
+  def start_workers(sup) do 
+    # Start the ListData worker
+    {:ok, list_data} = :supervisor.start_child(sup, worker(ListData, []))
+    # Now Start the SubSupervisor fot the actual ListServer
+    :supervisor.start_child(sup, worker(ListSubSupervisor, [list_data]))
+  end
+
+  def init(_) do
+    #child_processes = [ worker(ListServer, list) ]
+    supervise [], strategy: :one_for_one
+    #supervise child_processes, strategy: :one_for_one
   end
 end
